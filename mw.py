320a0b6ccae490b34f3de5d21368b310a78111cf
mimooh <mimoohowy@gmail.com>
2021-01-24 17:35:53 +0100

    null

--- mw.py
+++ mw.py
@@ -16,10 +16,26 @@ from include import Dump as dd
 
 class DojazdMW:
     def __init__(self):# {{{
+        self.s=Sqlite("mw.sqlite")
         self.json=Json()
-        f=self.json.read(sys.argv[1])
-        dd(f['fire'])
-        dd(len(f['vehicles']))
+        self.read_input()
+        self.mksqlite()
+# }}}
+    def read_input(self):# {{{
+        f=self.json.read('input.json')
+        for i in f:
+            print(i, i['scenariusz'])
+# }}}
+    def mksqlite(self):# {{{
+        self.s.query("drop table rozwiniecia")
+        self.s.query("create table rozwiniecia(rozwiniecie,opis,czas,osob)")
+        data=[]
+        data.append(['r1', 'jakiś opis', 120, 2 ]),
+        data.append(['r2', 'jakiś opis', 130, 2 ]),
+        data.append(['r3', 'jakiś opis', 140, 2 ]),
+
+        self.s.executemany('insert into rozwiniecia values ({})'.format(','.join('?' * len(data[0]))), data)
+        dd(self.s.query("select * from rozwiniecia where czas < 130"))
 # }}}
 
 DojazdMW()
