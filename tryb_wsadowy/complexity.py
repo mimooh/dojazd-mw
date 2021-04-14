a00b476f52eee2897358fd869dba83080f4ea44f
mimooh <mimoohowy@gmail.com>
2021-04-14 23:37:25 +0200

    null

--- tryb_wsadowy/complexity.py
+++ tryb_wsadowy/complexity.py
@@ -1,17 +1,14 @@
-# wynik
-# scenariuszy: 27000
-# godzin na wygenerowanie i przekazanie wariantów do MW (szacunkowy koszt 1 sekunda): 37.5
 
-x1=100 # miejsc pozarow
-x2=3 # maly duzy sredni (stałe promienie dymu i ognia czy losowane?)
-x3=10 # rozstawienia samochodów na linii
-x4=3 # zestawow samochodow: gba vs gba+drabina
-x5=3 # wyposazen samochodow: 100m węży vs 20m węży vs D10W vs noszaki itd.
+x1=100  # miejsc pozarow
+x2=3    # maly duzy sredni (stałe promienie dymu i ognia czy losowane?)
+x3=10   # rozstawień samochodów na linii
+x4=3    # zestawów samochodów: gba vs gba+drabina
+x5=1    # wyposażeń samochodów: 100m węży vs 20m węży vs D10W vs noszaki itd.
 
 scenariuszy=x1*x2*x3*x4*x5
-wariantow=5 # pathfindingów
+czas_generowania_scenariusza=30 # sekund
 
 print("scenariuszy:", scenariuszy)
-print("godzin na wygenerowanie i przekazanie wariantów do MW (szacunkowy koszt 1 wariant/s ):", scenariuszy*wariantow/3600)
+print("godzin na wygenerowanie i przekazanie wariantów do MW:", scenariuszy*czas_generowania_scenariusza/(3600*24), "dni")
 
 
