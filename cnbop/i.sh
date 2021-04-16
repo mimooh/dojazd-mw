f3617f984f8a208d298ca9285f9e4f8ef8a4abbd
mimooh <mimoohowy@gmail.com>
2021-04-16 12:28:42 +0200

    null

---
+++ cnbop/i.sh
@@ -0,0 +1,4 @@
+echo ".tables" | sqlite3 -header firetrucks.db
+echo "select * from firetrucks" | sqlite3 -header firetrucks.db
+echo "select * from FirefightingParameters" | sqlite3 -header firetrucks.db
+echo "select * from LadderLiftParameters" | sqlite3 -header firetrucks.db
