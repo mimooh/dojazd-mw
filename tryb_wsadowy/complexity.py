5bd8c97a249e28297d7d9837d0df2c7683bb81aa
mimooh <mimoohowy@gmail.com>
2021-04-17 19:00:19 +0200

    null

--- tryb_wsadowy/complexity.py
+++ tryb_wsadowy/complexity.py
@@ -1,11 +1,12 @@
 
 x1=100  # miejsc pozarow
-x2=3    # maly duzy sredni (stałe promienie dymu i ognia czy losowane?)
+x2=3    # maly duzy sredni 
 x3=10   # rozstawień samochodów na linii
 x4=3    # zestawów samochodów: gba vs gba+drabina
 x5=1    # wyposażeń samochodów: 100m węży vs 20m węży vs D10W vs noszaki itd.
+x6=1    # zmienność załogi (monte carlo)
 
-scenariuszy=x1*x2*x3*x4*x5
+scenariuszy=x1*x2*x3*x4*x5*x6
 czas_generowania_scenariusza=30 # sekund
 
 print("scenariuszy:", scenariuszy)
