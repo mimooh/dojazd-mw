aa892be7085e2b5c50aea14118d55c29d1792edd
mimooh <mimoohowy@gmail.com>
2021-04-23 10:40:31 +0200

    atrybut generic w tabeli firetrucks, dodatkowo csv/firetrucks.csv

--- sqlite/query.sh
+++ sqlite/query.sh
@@ -1,5 +1,5 @@
 echo ".tables"                              | sqlite3 -header firetrucks.db
-echo "select * from firetrucks"             | sqlite3 -header firetrucks.db
+echo "select * from FireTrucks"             | sqlite3 -header firetrucks.db
 echo "select * from FirefightingParameters" | sqlite3 -header firetrucks.db
 echo "select * from LadderLiftParameters"   | sqlite3 -header firetrucks.db
 echo "select * from Generics"               | sqlite3 -header firetrucks.db
